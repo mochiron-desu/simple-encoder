223d93715a6ffa809102036a3c0bf5f8d30b8edf
Suraj S <qwerky2003@gmail.com>
2022-07-14 22:28:06 +0530

    Added rot rotation with encoding and decoding

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 //source : https://stackoverflow.com/a/65390710/15023649
-function cipherEncode(str) {
-    str = str.toUpperCase();
-    return str.replace(/[A-Z]/g, rot13);
+function cipherEncode(str, rotnum) {
+    rotnum = parseInt(rotnum)
+    // str = str.toUpperCase();
+    str = str.replace(/[a-z]/g, rot13Lower);
+    return str.replace(/[A-Z]/g, rot13Upper);
 
-    function rot13(correspondance) {
+    function rot13Upper(correspondance) {
         const charCode = correspondance.charCodeAt();
         //A = 65, Z = 90
         return String.fromCharCode(
-            ((charCode + 13) <= 90) ? charCode + 13
-                : (charCode + 13) % 90 + 64
+            ((charCode + rotnum) <= 90) ? charCode + rotnum
+                : (charCode + rotnum) % 90 + 64
+        );
+
+    }
+    function rot13Lower(correspondance) {
+        const charCode = correspondance.charCodeAt();
+        //A = 97, Z = 122
+        return String.fromCharCode(
+            ((charCode + rotnum) <= 122) ? charCode + rotnum
+                : (charCode + rotnum) % 122 + 96
         );
 
     }
@@ -30,24 +41,71 @@ function removeFadeOut(el, speed) {
 //setting the document variables
 var inputText = document.querySelector("#userInput");
 const output = document.querySelector("#output");
+var rotnum = document.querySelector("#rotnum");
 
 //text processing function
 function textProcessing() {
     if (inputText.value.length == 0) {
         alert("Oi enter something you fool")
     }
+    else if (rotnum.value < 0 || rotnum.value > 26) {
+        alert("The range for value should be from 0-25")
+    }
     else {
-        inputText.value = (inputText.value).toUpperCase()
-        output.value = (cipherEncode(inputText.value));
+
+        // to encode
+        if (document.querySelector("#encode").checked) {
+            output.value = (cipherEncode(inputText.value, rotnum.value));
+        }
+
+        //to decode
+        else if (document.querySelector("#decode").checked) {
+            const rotnumvalue = rotnum.value;
+
+            //if rotnum is 13
+            if (rotnumvalue == 13) {
+                output.value = (cipherEncode(inputText.value, rotnum.value));
+            }
+
+            //if rotnum is a even number, then the loop must run 12 times
+            else if (rotnumvalue % 2 == 0) {
+                var newtext = inputText.value;
+                for (var i = 0; i < 12; i++) {
+                    newtext = (cipherEncode(newtext, rotnum.value));
+                    console.log(newtext)
+                }
+                output.value = newtext;
+            }
+
+            //if rotnum is odd number, then the loop must run 25 times
+            else if (rotnumvalue % 2 != 0) {
+                var newtext = inputText.value;
+                for (var i = 0; i < 25; i++) {
+                    newtext = (cipherEncode(newtext, rotnum.value));
+                    console.log(newtext)
+                }
+                output.value = newtext;
+            }
+        }
     }
 }
 
+rotnum.onkeyup = function () {
+    if (rotnum.value == 13) {
+        document.querySelector("#encode").disabled = true;
+        document.querySelector("#decode").disabled = true;
+    }
+    else if (rotnum.value != 13) {
+        document.querySelector("#encode").disabled = false;
+        document.querySelector("#decode").disabled = false;
+    }
+}
 
 //on key press change to uppercase and on enter do the function
 document.querySelector("#userInput").onkeyup = function () {
-    document.querySelector("#userInput").value = document.querySelector("#userInput").value.toUpperCase();
+    // document.querySelector("#userInput").value = document.querySelector("#userInput").value.toUpperCase();
 
-    if (event.key === "Enter"){
+    if (event.key === "Enter") {
         textProcessing();
     }
 }
